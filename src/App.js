fff8d510d3c9abbd86d9fb52d4bd7c44e5f259c0
re-nu <renukaborade08@gmail.com>
2021-12-22 20:36:31 +0530

    add to todo list

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import Checkbox from '@mui/material/Checkbox';
+import FormControlLabel from '@mui/material/FormControlLabel';
 
 function App() {
+  const [todo,settodo]=useState(["code kata","web kata","class tasks"])
+  const [addTodo,setaddTodo]=useState("")
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <TextField 
+       onChange={(event)=>setaddTodo(event.target.value)}
+       id="standard-basic" label="Add to todo list" variant="standard"
+      />
+      
+      <Button 
+       onClick={()=>settodo([...todo,addTodo])}
+       variant="outlined">Add
+      </Button>
+        {todo.map((td)=>(
+          <TodoList list={td}/>
+        ))}
     </div>
   );
 }
 
 export default App;
+
+function TodoList({list}) {
+  return(
+    <div className='todo-list'>
+      <FormControlLabel control={<Checkbox  />} label={list} />
+    </div>
+  )
+}
